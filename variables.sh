629257213e3c50427a77954ec1444a6655d457e3
phani <phalani1997@gmail.com>
2025-11-27 20:39:57 +0530

    added variables

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#no space for vairables after equal
+Person1=John
+Person2=Rahem
+echo "$Person1:: Hi $Person2, how are you"
+echo "$Person2:: Hello $Person1 am good ,how are you"
+echo "$Person1::what are you ,learning right now $Person2"
+echo "$Person2:: am learning devops $Person1"
